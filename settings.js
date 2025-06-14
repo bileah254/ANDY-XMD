f816c2f77c6aff9594447af7742a4432e4def1b9
bileah254 <bileah001@gmail.com>
2025-06-14 14:32:44 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'james md',
+  packname: 'andy mxd',
   author: '‎',
-  botName: "james md",
-  botOwner: 'james', // Your name
-  ownerNumber: '254704955033', //Set your number here without + symbol, just add country code & number without any space
+  botName: "andy mxd",
+  botOwner: 'andy', // Your name
+  ownerNumber: '254743454830', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
